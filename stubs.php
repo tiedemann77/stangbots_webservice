e1fd2f316d0528121efff4c18d8bab6fb38d9c56
Rafael <rafa.xt@protonmail.com>
2022-08-20 14:35:32 -0300

    Update stubs.php
    fix bug

--- stubs.php
+++ stubs.php
@@ -4,7 +4,7 @@
     <p>Esta ferramenta lista todos os esboços presentes em uma dada categoria na Wikipédia em Português.</p>
     <p>Considera apenas a categoria principal (não entra em subcategorias).</p>
     <br/>
-    <form action="/quality.php" type="GET">
+    <form action="/stubs.php" type="GET">
       <p>Categoria:</p>
       <input type="text" name="category" <?php if(isset($_GET['category'])){echo 'value="' . $_GET['category'] . '"';}?>>
 	  <br />
